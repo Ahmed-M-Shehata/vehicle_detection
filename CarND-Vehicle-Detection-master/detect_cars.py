d6974d4d55536011ff3c09f14b4e45a37e38cbd2
Ahmed Shehata <ahmedshehata@Ahmeds-MacBook-Pro.local>
2017-04-03 22:41:37 +0300

    adding the code

---
+++ CarND-Vehicle-Detection-master/detect_cars.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  1 11:39:51 2017
+
+@author: ahmedshehata
+"""
+
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+import numpy as np
+import glob
+import time
+from sklearn.svm import LinearSVC
+from sklearn.preprocessing import StandardScaler
+from help_func import *
+from sklearn.model_selection import train_test_split
+from scipy.ndimage.measurements import label
+from moviepy.editor import VideoFileClip
+ 
+# Read in cars and notcars
+cars = glob.glob('vehicles_smallset/*/*.jpeg')
+notcars = glob.glob('non-vehicles_smallset/*/*.jpeg')
+print(len(cars))
+print(len(notcars))
+
+# Define parameters for feature extraction
+color_space = 'HLS' # Can be RGB, HSV, LUV, HLS, YUV, YCrCb
+orient = 9  # HOG orientations
+pix_per_cell = 16 # HOG pixels per cell
+cell_per_block = 2 # HOG cells per block
+hog_channel = 1 # Can be 0, 1, 2, or "ALL"
+spatial_size = (16, 16) # Spatial binning dimensions
+hist_bins = 32    # Number of histogram bins
+spatial_feat = True # Spatial features on or off
+hist_feat = True # Histogram features on or off
+hog_feat = True # HOG features on or off
+y_start_stop = [400, 656] # Min and max in y to search in slide_window()
+
+car_features = extract_features(cars, color_space=color_space, 
+                        spatial_size=spatial_size, hist_bins=hist_bins, 
+                        orient=orient, pix_per_cell=pix_per_cell, 
+                        cell_per_block=cell_per_block, 
+                        hog_channel=hog_channel, spatial_feat=spatial_feat, 
+                        hist_feat=hist_feat, hog_feat=hog_feat)
+notcar_features = extract_features(notcars, color_space=color_space, 
+                        spatial_size=spatial_size, hist_bins=hist_bins, 
+                        orient=orient, pix_per_cell=pix_per_cell, 
+                        cell_per_block=cell_per_block, 
+                        hog_channel=hog_channel, spatial_feat=spatial_feat, 
+                        hist_feat=hist_feat, hog_feat=hog_feat)
+
+X = np.vstack((car_features, notcar_features)).astype(np.float64)                        
+# Fit a per-column scaler
+X_scaler = StandardScaler().fit(X)
+# Apply the scaler to X
+scaled_X = X_scaler.transform(X)
+
+# Define the labels vector
+y = np.hstack((np.ones(len(car_features)), np.zeros(len(notcar_features))))
+
+
+# Split up data into randomized training and test sets
+rand_state = np.random.randint(0, 100)
+X_train, X_test, y_train, y_test = train_test_split(
+    scaled_X, y, test_size=0.2, random_state=rand_state)
+
+print('Using:',orient,'orientations',pix_per_cell,
+    'pixels per cell and', cell_per_block,'cells per block')
+print('Feature vector length:', len(X_train[0]))
+# Use a linear SVC 
+svc = LinearSVC()
+# Check the training time for the SVC
+t=time.time()
+svc.fit(X_train, y_train)
+t2 = time.time()
+print(round(t2-t, 2), 'Seconds to train SVC...')
+# Check the score of the SVC
+print('Test Accuracy of SVC = ', round(svc.score(X_test, y_test), 4))
+# Check the prediction time for a single sample
+t=time.time()
+
+image = mpimg.imread('test_images/test5.jpg')
+draw_image = np.copy(image)
+test = classify_boxes(image, svc, X_scaler)
+result = draw_boxes(draw_image, test)
+heat = np.zeros_like(image[:,:,0]).astype(np.float)
+heat = add_heat(heat, test)
+heat = apply_threshold(heat, 3)
+heatmap = np.clip(heat, 0, 255)
+
+def process_frame(image):
+    windows = classify_boxes(image, svc, X_scaler)
+    heat = np.zeros_like(image[:,:,0]).astype(np.float)
+    heat = add_heat(heat, windows)
+    heat = apply_threshold(heat, 3)   
+    heatmap = np.clip(heat, 0, 255)
+    labels = label(heatmap)
+    draw_img = draw_labeled_bboxes(np.copy(image), labels)
+    
+    return draw_img
+im = process_frame(image)
+fig = plt.figure()
+plt.subplot(121)
+plt.imshow(im)
+plt.title('Detect car position')
+plt.subplot(122)
+plt.imshow(heatmap, cmap='gray')
+plt.title('heat map')
+
+
+
+video_output = 'result.mp4'
+clip1 = VideoFileClip("project_video.mp4")
+white_clip = clip1.fl_image(process_frame) 
+white_clip.write_videofile(video_output, audio=False)
